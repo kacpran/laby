4e1a90a3148473e0e5dfaabcfcc54ea399e17595
Student <Student@S527-LAB10>
2017-02-28 15:02:12 +0100

    new

---
+++ laby 1/laby 1/Source.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <typeinfo>
+#include <vector>
+
+using namespace std;
+
+
+class geshenk {
+public:
+	virtual void rozpakuj() {
+		cout << "ta daaa" << endl;
+	}
+
+	virtual void typ() {
+		cout << typeid(*this).name() << endl;
+
+	}
+};
+
+class schlapmyca : public geshenk {
+public:
+	void zaloz() {
+		cout << "jest ciepla" << endl;
+	}
+};
+
+class pieniadze : public geshenk {
+public:
+	void wydaj() {
+		cout << "teraz juz nic nie masz" << endl;
+	}
+};
+
+class czekolada : public geshenk {
+public:
+	void zjedz() {
+		cout << "zjadles" << endl;
+	}
+};
+
+int main() {
+
+	geshenk * geshenki;
+	vector<geshenk*> lista = {
+		new schlapmyca,
+		new pieniadze,
+		new czekolada,
+		new pieniadze
+	};
+
+	for (auto geshenk : lista) {
+
+		if (czekolada* nowe = dynamic_cast<czekolada*>(geshenk)) {
+			nowe->zjedz();
+			nowe->typ();
+		}
+
+		if (pieniadze* nowe = dynamic_cast<pieniadze*>(geshenk)) {
+			nowe->wydaj();
+			nowe->typ();
+		}
+
+		if (schlapmyca* nowe = dynamic_cast<schlapmyca*>(geshenk)) {
+			nowe->zaloz();
+			nowe->typ();
+		}
+	}
+
+	return 0;
+}
